677f87c967d8d432bad4cfc947a92565ac697baa
MD.SUJAN <csesujon155@gmail.com>
2022-12-30 05:36:38 +0600

    Fully Customize Logout Options

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Demo\DemoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +20,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Demo Controll
+Route::controller(DemoController::class)->group(function(){
+    Route::get('/about', 'Index')->name('about.page')->middleware('check');
+    Route::get('/contact', 'contactMethod')->name('contact.page');
+});
+
+// Admin All Route
+Route::controller(AdminController::class)->group(function(){
+    Route::get('/admin/logout', 'destroy')->name('admin.logout');
+});
+
+
+
+
+
 Route::get('/dashboard', function () {
     return view('admin.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
